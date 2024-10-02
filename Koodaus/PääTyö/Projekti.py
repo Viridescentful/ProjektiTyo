cbcda807e7ef3f8e36699acb0d6444cd5f6b8f4a
avakangas <ava.kangas@metropolia.fi>
2024-10-02 14:04:32 +0300

    moi

--- Koodaus/PääTyö/Projekti.py
+++ Koodaus/PääTyö/Projekti.py
@@ -1,1 +1,11 @@
 # Sup
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host='localhost',
+    port=3306,
+    database='some_database_name',
+    user='some_user_name',
+    password='some_password',
+    autocommit=True
+)
